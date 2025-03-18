86c5ac1d7140ce2518d85817b9f3b6754dd4eb85
xuruif <1251879294@qq.com>
2025-03-18 17:27:07 +0800

    Initial commit

---
+++ datasets/Drowsiness/label_name.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+@Time ： 2024/8/13 9:30
+@Auth ： 逗逗班学Python
+Bilibili：逗逗班学Python
+面包多: 逗逗班学Python
+@File ：label_name.py
+@IDE ：PyCharm
+功能：
+"""
+Chinese_name = {'Yawn': '打哈欠',
+                'close': '闭眼',
+                'noYawn': '未打哈欠',
+                'open': '睁眼'}
+Label_list = list(Chinese_name.values())
